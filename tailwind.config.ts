80e1424b443e9c2b21c5cb8dce98b054bbd9e646
Milad-Majidian <milad.majidian@yahoo.com>
2025-12-28 12:33:37 +0330

    feat: add base-container component styles and update Tailwind configuration for container settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,30 @@ const config: Config = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1680px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        lg: "2rem",
+        xl: "2.5rem",
+        "2xl": "3rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1680px", // Your custom max-width
+      },
+    },
     extend: {
         colors:tailwindTokens,
         borderRadius: {
